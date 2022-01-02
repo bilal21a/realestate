12846b7ec56f63dfac96edeb3d6fc2b3cf88649f
Bilal <b.anwar21.a@gmail.com>
2022-01-02 20:38:06 +0500

    aa

--- resources/lang/vendor/packages/seo-helper/mk/seo-helper.php
+++ resources/lang/vendor/packages/seo-helper/mk/seo-helper.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'meta_box_header' => 'Search Engine Optimize',
-    'edit_seo_meta' => 'Edit SEO meta',
-    'default_description' => 'Setup meta title & description to make your site easy to discovered on search engines such as Google',
-    'seo_title' => 'SEO Title',
-    'seo_description' => 'SEO description',
+    'meta_box_header' => 'Пребарување Engine Optimize',
+    'edit_seo_meta' => 'Уредете мета оптимизација',//Edit SEO meta
+    'default_description' => 'Поставете мета наслов и опис за да го направите вашиот сајт лесно да се открие на пребарувачите како што е Google',//Setup meta title & description to make your site easy to discovered on search engines such as Google
+    'seo_title' => 'Наслов на оптимизација',
+    'seo_description' => 'опис на оптимизација',//SEO description
 ];
